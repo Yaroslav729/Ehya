7d94efdaa4a153c89247851b1488b5be05130c03
Yaroslav729 <company-rosstan@yandex.ru>
2021-06-02 23:32:40 +0300

    create:header

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ehya</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto+Condensed:wght@700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="sass/style.css">
+  </head>
+  <body>
+    <header class="navbar">
+      <div class="container">
+        <div class="navbar__top">
+          <a href="index.php" class="logo">
+            <img src="img/logo.svg" alt="logo: Ehya" class="logo__image">
+          </a>
+          <div class="navbar__top-right">
+            <ul class="navbar-menu">
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Рекомендации</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Книги</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Блог</a></li>
+            </ul>
+            <!-- /.menu -->
+            <button class="button navbar__button">Получить</button>
+          </div>
+        </div>
+        <!-- /.navbar-menu__right -->
+        <!-- /.navbar-top -->
+      </div>
+      <!-- /.container -->
+
+    </header>
+
+  </body>
+</html>
